9ab11189fe363b1f29ffe4b03cc33c9ee7a45033
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-07-10 01:03:40 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_holidays_with_bbq(holiday_hash)
     holiday_hash.each do |season, holidays|
       holidays.each do |holiday, supplies|
         supplies.each do |supply|
-        if supplies == "BBQ"
+        if supply == "BBQ"
           bbq_holidays << holiday
         end
       end
